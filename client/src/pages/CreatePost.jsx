f24a7d758c39c7a80876a4b7626d2a3822011935
Jihoon Oh <jihoon.oh@edu.sait.ca>
2023-03-18 14:11:09 -0600

    fix createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -47,6 +47,31 @@ const CreatePost = () => {
     // }
   };
 
+  const generateImage = async () => {
+    // if (form.prompt) {
+    //   try {
+    //     setGeneratingImg(true);
+    //     const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify({
+    //         prompt: form.prompt,
+    //       }),
+    //     });
+    //     const data = await response.json();
+    //     setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+    //   } catch (err) {
+    //     alert(err);
+    //   } finally {
+    //     setGeneratingImg(false);
+    //   }
+    // } else {
+    //   alert('Please provide proper prompt');
+    // }
+  };
+
   const handleChange = (e) =>
     setForm({ ...form, [e.target.name]: e.target.value });
   return (
