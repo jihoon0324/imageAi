8c5d25c68c353b0721fae99d5dc1d04649b777dd
Jihoon Oh <jihoon.oh@edu.sait.ca>
2023-03-23 23:46:49 -0600

    add routers

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
 app.get("/", async (req, res) => {
   res.send("connect server ");
 });
