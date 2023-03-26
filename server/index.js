93bb0be3b7c2e8e2a0ad237720349028e58fa4b6
Jihoon Oh <jihoon.oh@edu.sait.ca>
2023-03-25 23:26:35 -0600

    add routers

--- server/index.js
+++ server/index.js
@@ -3,16 +3,16 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
-import postRoutes from "./routes/postRoutes.js";
-import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from "./routers/postRoutes.js";
+import dalleRoutes from "./routers/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.use("/api/v1/post", postRoutes);
-app.use("/api/v1/dalle", dalleRoutes);
+// app.use("/api/v1/post", postRoutes);
+// app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("connect server ");
